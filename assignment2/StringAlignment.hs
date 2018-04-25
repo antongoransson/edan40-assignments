c75afbfffff4c494e1064fbd36f92bf3404d6416
antongoransson <anton.goransson95@gmail.com>
2018-04-25 16:45:59 +0200

    Add helper function

--- assignment2/StringAlignment.hs
+++ assignment2/StringAlignment.hs
@@ -1,20 +1,22 @@
 module StringAlignment where
 
 type AlignmentType = (String,String)
---Observe that every alignment can start in exactly one of three ways:
---1 Two non-space characters
---2 Non-space above, space below
---3 Space above, non-space below
 scoreMatch = 0
 scoreMismatch = -1
 scoreSpace = -1
+
+
+stringScore :: String -> String -> Int
+stringScore [] [] = 0
+stringScore (s1:xs1) (s2:xs2) = score (s1, s2) + stringScore xs1 xs2
+
 
 optimalAlignments :: Int -> Int -> Int -> String -> String -> [AlignmentType]
 optimalAlignments _ _ _ _ _= [("ABC ","ACBC ")]
 
 similarityScore :: String -> String -> Int
-similarityScore [] _ = 0
-similarityScore _ [] = 0
+similarityScore [] _ = scoreSpace
+similarityScore _ [] = scoreSpace
 similarityScore (x:xs) (y:ys) = maximum [similarityScore xs ys + score (x, y),
                               similarityScore (x:xs) ys + score ('-', y),
                               similarityScore xs (y:ys) + score (x, '-')]
