51a0a11017c78d4960587e702565f2fcb30d144a
antongoransson <anton.goransson95@gmail.com>
2018-04-26 11:21:28 +0200

    Update similarityscore

--- assignment2/StringAlignment.hs
+++ assignment2/StringAlignment.hs
@@ -14,16 +14,34 @@ stringScore ((s1:xs1), (s2:xs2)) = score (s1, s2) + stringScore (xs1, xs2)
 optimalAlignments :: Int -> Int -> Int -> String -> String -> [AlignmentType]
 optimalAlignments _ _ _ _ _= [("ABC ","ACBC ")]
 
+-- similarityScore :: String -> String -> Int
+-- similarityScore [] _ = scoreSpace
+-- similarityScore _ [] = scoreSpace
+-- similarityScore (x:xs) (y:ys) = maximum [similarityScore xs ys + score (x, y),
+--                               similarityScore (x:xs) ys + score ('-', y),
+--                               similarityScore xs (y:ys) + score (x, '-')]
+
+
 similarityScore :: String -> String -> Int
-similarityScore [] _ = scoreSpace
-similarityScore _ [] = scoreSpace
-similarityScore (x:xs) (y:ys) = maximum [similarityScore xs ys + score (x, y),
-                              similarityScore (x:xs) ys + score ('-', y),
-                              similarityScore xs (y:ys) + score (x, '-')]
+similarityScore xs ys = simScore (length xs) (length ys)
+  where
+     simScore i j = simscoreTable !! i !! j
+     simscoreTable = [[ simEntry i j | j <- [0..]] | i <- [0..] ]
+
+     simEntry :: Int -> Int -> Int
+     simEntry 0 0 = 0
+     simEntry _ 0 = scoreSpace
+     simEntry 0 _ = scoreSpace
+     simEntry i j
+       | x == y    = scoreMatch + simScore (i - 1) (j - 1)
+       | otherwise = max (scoreMismatch + simScore i (j - 1)) (scoreMismatch + simScore (i - 1) j)
+       where
+          x = xs!!(i - 1)
+          y = ys!!(j - 1)
 
 --Appends(:) h1 to the first lits and h2 to the seconds list, for all pairs in a list.
 attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
-attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
+attachHeads h1 h2 aList = [(h1:xs, h2:ys) | (xs, ys) <- aList]
 
 --maximaBy length ["cs", "efd", "lth", "it"] should return ["efd", "lth"].
 maximaBy :: Ord b => (a -> b) -> [a] -> [a]
@@ -61,3 +79,5 @@ score (x, y)
 
 string1 = "writers"
 string2 = "vintner"
+string3 = "bananrepubliksinvasionsarmestabsadjutant"
+string4 = "kontrabasfiolfodralmakarmästarlärling"
