d1cc37a1db4f509a0ab9416dee99fdeace9e387e
antongoransson <anton.goransson95@gmail.com>
2018-05-03 13:10:40 +0200

    Add some setup

---
+++ sudoku/test.py
@@ -0,0 +1,14 @@
+def cross(A, B):
+    "Cross product of elements in A and elements in B."
+    return [a+b for a in A for b in B]
+
+digits   = '123456789'
+rows     = 'ABCDEFGHI'
+cols     = digits
+squares  = cross(rows, cols)
+unitlist:: [String]
+unitlist = ([cross(rows, c) for c in cols] +
+            [cross(r, cols) for r in rows] +
+            [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')])
+
+print(unitlist)
