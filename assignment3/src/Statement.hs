d1995fb434a288bae52c07f6b4777754ddbd03be
antongoransson <anton.goransson95@gmail.com>
2018-05-15 16:07:36 +0200

    Add statement functions

--- assignment3/src/Statement.hs
+++ assignment3/src/Statement.hs
@@ -3,21 +3,42 @@ import Prelude hiding (return, fail)
 import Parser hiding (T)
 import qualified Dictionary
 import qualified Expr
+
 type T = Statement
 data Statement =
     Assignment String Expr.T |
-    If Expr.T Statement Statement
+    If Expr.T Statement Statement |
+    Begin [Statement] |
+    While Expr.T Statement | 
+    Read String |
+    Write Expr.T |
+    Skip
     deriving Show
 
 assignment = word #- accept ":=" # Expr.parse #- require ";" >-> buildAss
 buildAss (v, e) = Assignment v e
 
+skip =  accept "skip" #- require "skip;" >-> buildSkip
+buildSkip _ = Skip
+
+begin = accept "begin" -# iter (parse #- spaces) #- require "end;" >-> Begin
+
+while = accept "while" -# Expr.parse #- require "do" >-> While
+
+read =  accept "read" -# word #- require ";" >-> Read
+
+write = accept "write" -# Expr.parse #- require ";" >-> Write
+
+ifElse = accept "if" -# Expr.parse #- require "then" # parse #- require "else" # parse >-> buildIf
+buildIf ((a, b), c) = If a b c
+
 exec :: [T] -> Dictionary.T String Integer -> [Integer] -> [Integer]
 exec (If cond thenStmts elseStmts: stmts) dict input = 
-    if (Expr.value cond dict)>0 
+    if Expr.value cond dict > 0 
     then exec (thenStmts: stmts) dict input
     else exec (elseStmts: stmts) dict input
 
 instance Parse Statement where
   parse = error "Statement.parse not implemented"
   toString = error "Statement.toString not implemented"
+
