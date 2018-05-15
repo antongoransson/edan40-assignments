c3483cbf0a3f1d88a45108adb22134a189a4d58a
antongoransson <anton.goransson95@gmail.com>
2018-05-15 17:03:31 +0200

    Add exec in statement

--- assignment3/src/Statement.hs
+++ assignment3/src/Statement.hs
@@ -15,17 +15,20 @@ data Statement =
     Skip
     deriving Show
 
+statement = skip ! assignment ! begin ! ifElse ! while ! read' ! write
+
 assignment = word #- accept ":=" # Expr.parse #- require ";" >-> buildAss
 buildAss (v, e) = Assignment v e
 
-skip =  accept "skip" #- require "skip;" >-> buildSkip
+skip = accept "skip" #- require "skip;" >-> buildSkip
 buildSkip _ = Skip
 
 begin = accept "begin" -# iter (parse #- spaces) #- require "end;" >-> Begin
 
-while = accept "while" -# Expr.parse #- require "do" >-> While
+while = accept "while" -# Expr.parse #- require "do" # parse >-> buildWhile
+buildWhile (a, b) = While a b
 
-read =  accept "read" -# word #- require ";" >-> Read
+read' = accept "read" -# word #- require ";" >-> Read
 
 write = accept "write" -# Expr.parse #- require ";" >-> Write
 
@@ -37,8 +40,17 @@ exec (If cond thenStmts elseStmts: stmts) dict input =
     if Expr.value cond dict > 0 
     then exec (thenStmts: stmts) dict input
     else exec (elseStmts: stmts) dict input
+exec (Assignment v e : stmts) dict input = exec stmts (Dictionary.insert(v, Expr.value e dict) dict) input    
+exec (While cond wStmts : stmts) dict input = 
+    if Expr.value cond dict > 0 
+    then exec (wStmts: While cond wStmts : stmts) dict input
+    else exec stmts dict input
+exec (Begin bStmts : stmts) dict input = exec (bStmts ++ stmts) dict input
+exec (Read s : stmts) dict (i:input) = exec stmts (Dictionary.insert(s, i) dict) input 
+exec (Write e: stmts) dict input =  Expr.value e dict : exec stmts dict input 
+exec (Skip : stmts) dict input = exec stmts dict input
 
 instance Parse Statement where
-  parse = error "Statement.parse not implemented"
+  parse = statement
   toString = error "Statement.toString not implemented"
 
