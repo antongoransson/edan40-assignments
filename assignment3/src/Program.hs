09be35eeeea9993b6f48608687e611817abdb38f
antongoransson <anton.goransson95@gmail.com>
2018-05-16 12:41:13 +0200

    Fix program parse

--- assignment3/src/Program.hs
+++ assignment3/src/Program.hs
@@ -3,9 +3,10 @@ import Parser hiding (T)
 import qualified Statement
 import qualified Dictionary
 import Prelude hiding (return, fail)
-newtype T = Program () -- to be defined
+newtype T = Program [Statement.T]
+
 instance Parse T where
-  parse = error "Program.parse not implemented"
+  parse = iter Statement.parse >-> Program
   toString = error "Program.toString not implemented"
              
-exec = error "Program.exec not implemented"
+exec (Program p) = Statement.exec p Dictionary.empty
